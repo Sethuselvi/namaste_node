85bad01142ef7b57a5eafda44d323d18e69b2eab
Sethuselvi <sethuselvi-98@gmail.com>
2025-12-12 16:44:42 +0530

    Used destructuring for accessing variables and functions from modules

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const obj = require('./sum.js')
+const {x,calculateSum} = require('./sum.js')
 require('./xyz.js')
 var name = "Namaste Nodejs";
 var a= 10;
 var b=20;
 console.log(name);
 console.log(a+b);
-obj.calculateSum(a,b);
-console.log(obj.x);
+calculateSum(a,b);
+console.log(x);
 // console.log(global);
 // console.log(this);
 // console.log(globalThis===global);
