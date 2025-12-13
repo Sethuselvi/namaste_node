4e488ead3777ebf684028ffadff40d7347b370cd
Sethuselvi <sethuselvi-98@gmail.com>
2025-12-13 18:20:40 +0530

    importing json data and util from node js core module

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 // const {calculateMultiply} = require('./calculate/multiply.js')
 const {calculateSum,calculateMultiply} = require('./calculate')
 require('./xyz.js')
+const data = require('./data.json')
+const util = require('node:util');
 var name = "Namaste Nodejs";
 var a= 10;
 var b=20;
@@ -9,6 +11,8 @@ console.log(name);
 console.log(a+b);
 calculateSum(a,b);
 calculateMultiply(a,b);
+console.log(data);
+console.log(util);
 // console.log(x);
 // console.log(global);
 // console.log(this);
