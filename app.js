a08db44d343c411410cd1e68a0d213802f037f1f
Sethuselvi <sethuselvi-98@gmail.com>
2025-12-13 17:22:35 +0530

    Added a folder as a module

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-const {x,calculateSum} = require('./calculate/sum.js')
-const {calculateMultiply} = require('./calculate/multiply.js')
+// const {x,calculateSum} = require('./calculate/sum.js')
+// const {calculateMultiply} = require('./calculate/multiply.js')
+const {calculateSum,calculateMultiply} = require('./calculate')
 require('./xyz.js')
 var name = "Namaste Nodejs";
 var a= 10;
@@ -8,7 +9,7 @@ console.log(name);
 console.log(a+b);
 calculateSum(a,b);
 calculateMultiply(a,b);
-console.log(x);
+// console.log(x);
 // console.log(global);
 // console.log(this);
 // console.log(globalThis===global);
