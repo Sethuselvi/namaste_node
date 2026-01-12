1f384dccd50f83c9d593f1292acbbc3bf15ff4f6
Sethuselvi <sethuselvi-98@gmail.com>
2026-01-12 14:27:24 +0530

    Created Mongodb databse nd performed insertion and finding documents

---
+++ database.js
@@ -0,0 +1,37 @@
+const{MongoClient} = require('mongodb');
+// Connection URL
+const url = 'mongodb+srv://sethuselvi:1234@nodetuts.73kic02.mongodb.net/?appName=nodetuts'
+const client = new MongoClient(url);
+async function main(){
+    // Use connect method to connect to the server
+    await client.connect();
+    console.log('Connected successfully to server');
+    const db = client.db('Helloworld')
+const collection = db.collection('User')
+const data ={
+    firstname: 'Arulan',
+    lastname: 'Sathuguru',
+    city: 'Chennai',
+    country: 'India',
+    phone_number: '97463544646'
+}
+//Insert many
+// const insertResult = await collection.insertMany([data]);
+// console.log('Inserted documents =>', insertResult);
+//InsertOne
+const insertResult = await collection.insertOne(data);
+console.log('Inserted documents =>', insertResult);
+const findResult = await collection.find({}).toArray();
+console.log('Found documents =>', findResult);
+const result = await collection.find({firstname: 'Deepika'}).toArray();
+console.log('Found documents =>', result);
+const countResult = await collection.countDocuments({});
+console.log('Count of the documents in user collection =>', countResult);
+
+
+return 'Done!'
+}
+//When the promise resolves successfully, the resolved value is automatically passed as an argument to the console.log function
+main().then(console.log).catch(console.error).finally(()=>client.close())
+
+
